b45df55ad6b6abca6ca41a1f061b81da5170bae3
Alan Forneron <alan.forneron@naranja.com>
2023-05-18 08:41:17 -0300

    first commit

---
+++ WeatherTeco/WeatherTeco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTeco
+//
+//  Created by Alan Forneron on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
